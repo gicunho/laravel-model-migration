30d49611ae3518200b315f2387c2ceda8ef39817
gicunho <gicunho@hotmail.it>
2021-07-13 16:01:44 +0200

    Creato Controller collegato .env al DB creato tabella con il terminale creato modello

---
+++ blog/database/migrations/2021_07_13_132545_create_trips_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('location');
+            $table->string('hotel_name');
+            $table->DATE('departure_date');
+            $table->DATE('return_date');
+            $table->smallInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
